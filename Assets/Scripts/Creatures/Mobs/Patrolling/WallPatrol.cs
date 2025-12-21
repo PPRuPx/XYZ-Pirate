8c1e039072183516799f38f8b551b11f6c27a169
PPRuPx <pprupx@outlook.com>
2025-12-22 02:47:29 +0300

    PW-2: New patrolling type

---
+++ Assets/Scripts/Creatures/Mobs/Patrolling/WallPatrol.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using Components.ColliderBased;
+using UnityEngine;
+
+namespace Creatures.Mobs.Patrolling
+{
+    public class WallPatrol : Patrol
+    {
+        [SerializeField] private LayerCheck _wallCheck;
+        [SerializeField] private int _direction = 1;
+        
+        private Creature _creature;
+        
+        private void Awake()
+        {
+            _creature = GetComponent<Creature>();
+        }
+        
+        public override IEnumerator DoPatrol()
+        {
+            while (enabled)
+            {
+                if (!_wallCheck.IsTouchingLayers)
+                {
+                    _creature.SetDirection(new Vector2(_direction, 0));
+                }
+                else
+                {
+                    _direction = -_direction;
+                    _creature.SetDirection(new Vector2(_direction, 0));
+                }
+
+                yield return null;
+            }
+        }
+    }
+}
